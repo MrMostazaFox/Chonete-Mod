98df0f0841617b6c2856efa853cbb8d54f37786c
MrMostazaFox <107527735+MrMostazaFox@users.noreply.github.com>
2022-06-14 22:03:00 -0600

    Add files via upload

---
+++ ChoneteMod/Items/Chonete.cs
@@ -0,0 +1,40 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ChoneteMod.Items
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class Chonete : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			DisplayName.SetDefault("Chonete"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("¡Picha!");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.width = 20;
+			item.height = 20;
+			item.value = 1200;
+			item.rare = 0;
+			item.vanity = true;
+		}
+
+		public override void DrawHair(ref bool drawHair, ref bool drawAltHair)
+		{
+			drawAltHair = true;
+		}
+
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Silk, 2 );
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
